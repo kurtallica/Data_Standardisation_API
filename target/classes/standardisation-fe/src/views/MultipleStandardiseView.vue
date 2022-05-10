856fdd66d891be9ce7cbe409123e8f639b6169e6
Kurtis Fleming <kurtallica@hotmail.com>
2022-05-10 14:54:22 +0100

    Updated directory/Updated SQL queries.txt/fresh maven build

--- target/classes/standardisation-fe/src/views/MultipleStandardiseView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="multiple-standardise">
-    <h1>This is the Multiple Standardisation page</h1>
-  </div>
-</template>
--- target/classes/standardisation-fe/src/views/SingleStandardiseView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="single-standardise">
-    <h1>This is the Signle Standardisation page</h1>
-  </div>
-</template>
